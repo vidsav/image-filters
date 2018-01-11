139a7a0c5a23c75bcd9b36a0e049ae94401da3cd
vidsav <savnikvid5@gmail.com>
2018-01-11 13:56:33 +0100

    saturate effect

---
+++ gallery.php
@@ -0,0 +1,44 @@
+    <div class="row">
+        <div class="col-lg-3 col-md-3 col-sm-3">
+            <a href="#">
+                <img src="Images/1.jpg" class="img-fluid img-thumbnail">
+            </a>
+        </div>
+               <div class="col-lg-3 col-md-3 col-sm-3">
+            <a href="#">
+                <img src="Images/2.jpg" class="img-fluid img-thumbnail">
+            </a>
+        </div>
+               <div class="col-lg-3 col-md-3 col-sm-3">
+            <a href="#">
+                <img src="Images/3.jpg" class="img-fluid img-thumbnail">
+            </a>
+        </div>
+               <div class="col-lg-3 col-md-3 col-sm-3">
+            <a href="#">
+                <img src="Images/4.jpg" class="img-fluid img-thumbnail">
+            </a>
+        </div>
+    </div><br>
+        <div class="row">
+        <div class="col-lg-3 col-md-3 col-sm-6">
+            <a href="#">
+                <img src="Images/5.jpg" class="img-fluid img-thumbnail">
+            </a>
+        </div>
+               <div class="col-lg-3 col-md-3 col-sm-6">
+            <a href="#">
+                <img src="Images/6.jpg" class="img-fluid img-thumbnail">
+            </a>
+        </div>
+               <div class="col-lg-3 col-md-3 col-sm-6">
+            <a href="#">
+                <img src="Images/7.jpg" class="img-fluid img-thumbnail">
+            </a>
+        </div>
+               <div class="col-lg-3 col-md-3 col-sm-6">
+            <a href="#">
+                <img src="Images/8.jpg" class="img-fluid img-thumbnail">
+            </a>
+        </div>
+    </div>
---
+++ index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title></title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">CSS3</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Basic filters
+        </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#gray">Gray</a>
+                        <a class="dropdown-item" href="#blur">Blur</a>
+                        <a class="dropdown-item" href="#sepia">Sepia</a>
+                        <a class="dropdown-item" href="#invert">Invert</a>
+                        <a class="dropdown-item" href="#opacity">Opacity</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Advanced filters
+        </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#saturate">Saturate</a>
+                        <a class="dropdown-item" href="#blur">Blur</a>
+                        <a class="dropdown-item" href="#sepia">Sepia</a>
+                        <a class="dropdown-item" href="#invert">Invert</a>
+                        <a class="dropdown-item" href="#opacity">Opacity</a>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <div class="container image-gallery">
+        <div class="col-md-12 text-center">
+            <h2>BASIC FILTERS</h2>
+        </div>
+        <hr>
+        <br>
+        <h4>Original</h4>
+        <hr>
+        <?php include('gallery.php'); ?>
+    </div>
+    <div class="container image-gallery" id="gray">
+        <h4>Gray filter</h4>
+        <hr>
+        <?php include('gallery.php'); ?>
+    </div>
+    <div class="container image-gallery" id="blur">
+        <h4>Blur filter</h4>
+        <hr>
+        <?php include('gallery.php'); ?>
+    </div>
+    <div class="container image-gallery" id="sepia">
+        <h4>Sepia filter</h4>
+        <hr>
+        <?php include('gallery.php'); ?>
+    </div>
+    <div class="container image-gallery" id="invert">
+        <h4>Invert filter</h4>
+        <hr>
+        <?php include('gallery.php'); ?>
+    </div>
+    <div class="container image-gallery" id="opacity">
+        <h4>Opacity filter</h4>
+        <hr>
+        <?php include('gallery.php'); ?>
+    </div>
+    <div class="container image-gallery" id="saturate">
+        <div class="col-md-12 text-center">
+            <h2>ADVANCED FILTERS</h2>
+        </div>
+        <hr>
+        <br>
+        <h4>Saturate filter</h4>
+        <hr>
+        <?php include('gallery.php'); ?>
+    </div>
+</body>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
+
+</html>
